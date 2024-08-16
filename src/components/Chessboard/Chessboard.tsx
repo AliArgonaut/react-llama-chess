ee9298dff66f8073815aa69266427763e3edab56
Lloyd <Lloydabonds@gmail.com>
2024-08-16 03:25:55 -0400

    resolved chessboard tile error

--- src/components/Chessboard/Chessboard.tsx
+++ src/components/Chessboard/Chessboard.tsx
@@ -55,7 +55,7 @@ export default function Chessboard() {
                 }
  
             })
-            board.push(<Tile image={image} number={number}/>)
+            board.push(<Tile key={`${j},${i}`} image={image} number={number}/>)
         }
     }
 
